b14ba090c31ac6e332196d0eb97d41cb2f27a504
Kanat <bomd2@MacBook-Air-Kanat.local>
2022-06-21 18:52:24 +0600

    create account register with twilio

---
+++ test.py
@@ -0,0 +1,20 @@
+"""
+
+
+djangorestframework-simplejwt - регистрация на другой основе
+отличие от предыдущего проекта:
+1) письмо на телефон 
+2) сэлэри 
+3) логин на других токенов(jwt tokens)
+
+
+
+в инсталд аппс вторым передаем
+    'rest_framework_simplejwt',
+
+
+ Настраиваем .env 
+TWILIO_SID=ACd86d0bdbe5407e6a41f28501c13630fd
+TWILIO_TOKEN=7c0162fb617f3d001227d533fa11b53f
+TWILIO_NUMBER=
+"""
